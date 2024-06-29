267949bca1d8ea407bceb631579aec7000e6de79
Sagnik Dey <146818206+sagnik-create@users.noreply.github.com>
2024-06-29 15:01:12 +0000

    Task 1 Finished

--- Task 1/Script.js
+++ Task 1/Script.js
@@ -9,7 +9,23 @@ btn = addEventListener("click",()=>{
 })
 
 function Chatting(){
-    if (User = "Hi, My name is Sagnik Dey") {
+    if (User == "Hi, My name is Sagnik Dey") {
         return Chattxt.innerHTML = "Hi how can I help you?"
     }
+    else if (User == "How are you?"){
+        return Chattxt.innerHTML = "I am fine"
+    }
+    else if (User == "What is the Capital of India?"){
+        return Chattxt.innerHTML = "Delhi"
+    }
+    else if (User == "What is your Name?"){
+        return Chattxt.innerHTML = "My name is ChatBot"
+    }
+    else if (User == ""){
+        return Chattxt.innerHTML = ""
+    }
+    else {
+        return Chattxt.innerHTML = "Didn't get that"
+    }
+
 }
