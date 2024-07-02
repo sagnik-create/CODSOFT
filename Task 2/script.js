14d49a620a249a882319254c74ab394ee090e4ff
Sagnik Dey <146818206+sagnik-create@users.noreply.github.com>
2024-07-02 18:06:47 +0000

    basic ideation

---
+++ Task 2/script.js
@@ -0,0 +1,29 @@
+// DOM definitions
+b1 = document.getElementById("b1")
+b2 = document.getElementById("b2")
+b3 = document.getElementById("b3")
+b4 = document.getElementById("b4")
+b5 = document.getElementById("b5")
+b6 = document.getElementById("b6")
+b7 = document.getElementById("b7")
+b8 = document.getElementById("b8")
+b9 = document.getElementById("b9")
+
+b1btn = b1.value = "X"
+
+// function movechecker(){
+//     for (let index = 0; index < 9; index++) {
+//         let flag = 1;
+//         if (flag == 1) {
+//             team = 'X'
+//             flag = 0;
+//         }
+
+//         else( ) {
+//             team = 'O'
+//             flag = 1;
+//         }
+        
+//     }
+// }
+
